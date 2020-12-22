196bcc0085b2110d0275a672b2ef8647698a0c2b
Pablo Torres Searle <pablo@MacBook-Pro-de-Pablo.local>
2020-12-22 18:42:54 +0100

    ft_server

--- mini-rt/main.c
+++ mini-rt/main.c
@@ -6,22 +6,56 @@
 /*   By: pablo <pablo@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/21 19:14:12 by pablo             #+#    #+#             */
-/*   Updated: 2020/12/21 22:42:10 by pablo            ###   ########.fr       */
+/*   Updated: 2020/12/22 11:22:46 by pablo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "mini_rt.h"
 
+void draw_square(t_config config, int x, int y, int width)
+{
+    int i;
+    int j;
+
+    i = 0;
+    j = 0;
+    while (i < width)
+    {
+        mlx_pixel_put(config.mlx, config.mlx_win, x + i, y, 233);
+        mlx_pixel_put(config.mlx, config.mlx_win, x, y + i, 233);
+        mlx_pixel_put(config.mlx, config.mlx_win, x + i, y + width, 233);
+        mlx_pixel_put(config.mlx, config.mlx_win, x + width, y + i, 233);
+        i++;
+    }
+    while (j < width / 2.7)
+    {
+        mlx_pixel_put(config.mlx, config.mlx_win, x + j, y + j, 233);
+        mlx_pixel_put(config.mlx, config.mlx_win, x + width + j, y + j, 233);
+        mlx_pixel_put(config.mlx, config.mlx_win, x + width + j, y + width + j, 233);
+        mlx_pixel_put(config.mlx, config.mlx_win, x + j, y + width + j, 233);
+
+        j++;
+    }
+    i = 0;
+    x += j;
+    y += j;
+    while (i < width)
+    {
+        mlx_pixel_put(config.mlx, config.mlx_win, x + i, y, 233);
+        mlx_pixel_put(config.mlx, config.mlx_win, x, y + i, 233);
+        mlx_pixel_put(config.mlx, config.mlx_win, x + i, y + width, 233);
+        mlx_pixel_put(config.mlx, config.mlx_win, x + width, y + i, 233);
+        i++;
+    }
+}
 
 int main()
 {
-    void *mlx;
-    void *mlx_win;
+    t_config config;
 
+    config.mlx = mlx_init();
+    config.mlx_win = mlx_new_window(config.mlx, 900, 600, "hola");
+    draw_square(config, 50, 50, 100);
 
-
-    mlx = mlx_init();
-    mlx_win = mlx_new_window(mlx,900,600,"hola");
-    mlx_loop(mlx);
+    mlx_loop(config.mlx);
 }
-
--- mini-rt/mini_rt.h
+++ mini-rt/mini_rt.h
@@ -6,12 +6,23 @@
 /*   By: pablo <pablo@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/21 19:15:09 by pablo             #+#    #+#             */
-/*   Updated: 2020/12/21 19:28:01 by pablo            ###   ########.fr       */
+/*   Updated: 2020/12/22 10:42:17 by pablo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "./libft/libft.h"
+# include "./libft/libft.h"
+# include <mlx.h>
+# include <stdio.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <math.h>
 
-#include <mlx.h>
+typedef struct	s_conifg
+{
+    void *mlx;
+    void *mlx_win;
+    
+}               t_config;
+
 
 
