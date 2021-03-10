e018a241d03b4e0ab85e26bdd6c827e4cf26b5d6
Pablo Torres <ptorres@c1r13s6.42madrid.com>
2021-03-10 18:19:55 +0100

    load balancer?

--- ft_services/setup.sh
+++ ft_services/setup.sh
@@ -1,32 +1,46 @@
 #!/bin/bash
 
-Black='\033[0;30m'        # Black
-Red='\033[0;31m'          # Red
-Green='\033[0;32m'        # Green
-Yellow='\033[0;33m'       # Yellow
-Blue='\033[0;34m'         # Blue
-Purple='\033[0;35m'       # Purple
-Cyan='\033[0;36m'         # Cyan
-White='\033[0;37m' 
+Black='\033[0;30m'        
+Red='\033[0;31m'         
+Green='\033[0;32m'      
+Yellow='\033[0;33m'    
+Blue='\033[0;34m'     
+Purple='\033[0;35m'
+Cyan='\033[0;36m'
+White='\033[0;37m'
 endColour='\033[0m'
 
+#start minikube
 echo "\n${Green}Minikube starting...${endColour}"
-
 minikube --vm-driver=docker start
-
 minikube addons enable dashboard
 
-echo "Building Nginx"
+#metalLB instalation
+echo "\n${Green}Installing metalLB...${endColour}"
+kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.5/manifests/namespace.yaml
+kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.5/manifests/metallb.yaml
+kubectl create secret generic -n metallb-system memberlist --from-literal=secretkey="$(openssl rand -base64 128)"
+#configuration file load balancer 
+kubectl apply -f srcs/metalLB.yml
 
+
+#building docker images
+echo "${Green}Building images${endColour}"
 docker build -t my_nginx ./srcs/nginx
 
-echo "Deployment of Nginx"
+echo "${Green}Deployment of Nginx${endColour}"
+kubectl create -f ./srcs/nginx/nginx.yml 
 
 
-kubectl create -f ./srcs/nginx/nginx.yml --validate=false
 
-echo "Starting dashboard...."
 
-minikube dashboard
+
+#to run docker registry on local 
+eval $(minikube docker-env)
+
+#open k8s dashboard
+echo "\n${Green}Starting dashboard....${endColour}"
+#minikube dashboard
+
 
 
