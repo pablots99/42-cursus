8fa82d6fc6d49291e13d36951bb617b569cbb032
Pablo Torres <ptorres@c1r13s6.42madrid.com>
2021-03-15 12:54:44 +0100

    no avances

--- ft_services/setup.sh
+++ ft_services/setup.sh
@@ -10,29 +10,35 @@ Cyan='\033[0;36m'
 White='\033[0;37m'
 endColour='\033[0m'
 
-
+#delete previous minikube
+echo "\n${Green}Deleting previous minikube...${endColour}"
+#minikube stop
+minikube delete
+rm -rf ~/.minikube
 
 
 #start minikube
-echo "\n${Green}Minikube starting...${endColour}"
-minikube --vm-driver=docker start
+echo "\n${Green}Starting Minikube...${endColour}"
+minikube start 
 minikube addons enable dashboard
 minikube addons enable metallb
 
 
 
+eval $(minikube -p minikube docker-env)
+
 #metalLB instalation && preparation 
-kubectl get configmap kube-proxy -n kube-system -o yaml | \
-sed -e "s/strictARP: false/strictARP: true/" | \
-kubectl apply -f - -n kube-system
+# kubectl get configmap kube-proxy -n kube-system -o yaml | \
+# sed -e "s/strictARP: false/strictARP: true/" | \
+# kubectl apply -f - -n kube-system
+
 
 echo "\n${Green}Installing metalLB...${endColour}"
 kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.5/manifests/namespace.yaml
 kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.5/manifests/metallb.yaml
 kubectl create secret generic -n metallb-system memberlist --from-literal=secretkey="$(openssl rand -base64 128)"
-
-
-
+kubectl apply -f srcs/loadbalancer/metallb-config.yml
+kubectl apply -f srcs/loadbalancer/service.yml
 
 
 #building docker images
@@ -43,14 +49,8 @@ docker build -t my_nginx ./srcs/nginx --network host
 echo "${Green}Minikube Deployment and Services${endColour}"
 kubectl apply -f srcs/nginx/nginx.yml
 
-#configuration file load balancer 
-kubectl apply -f srcs/loadbalancer/metallb-config.yml
-kubectl apply -f srcs/loadbalancer/service.yml
-
 
 #open k8s dashboard
 echo "\n${Green}Starting dashboard....${endColour}"
 #minikube dashboard
-
-
 
