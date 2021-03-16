013ac23e79255c11160d7edf54ae661b69f74da3
Pablo Torres Searle <pablo@MacBook-Pro-de-Pablo.local>
2021-03-16 01:00:00 +0100

    error on ftps server

--- ft_services/setup.sh
+++ ft_services/setup.sh
@@ -10,7 +10,6 @@ Cyan='\033[0;36m'
 White='\033[0;37m'
 endColour='\033[0m'
 
-<<<<<<< HEAD
 
 echo "\n${Green}Deleting previous minikube...${endColour}"
 #delete previous minikube
@@ -23,38 +22,12 @@ rm -rf ~/.minikube
 echo "\n${Green}Minikube starting...${endColour}"
 minikube start --cpus=2 --disk-size 11000 --vm-driver virtualbox 
 minikube addons enable metrics-server
-=======
-#delete previous minikube
-echo "\n${Green}Deleting previous minikube...${endColour}"
-#minikube stop
-minikube delete
-rm -rf ~/.minikube
-
-
-#start minikube
-echo "\n${Green}Starting Minikube...${endColour}"
-minikube start 
->>>>>>> e3c6c445707464786619b696469b758e7fd9e7f5
 minikube addons enable dashboard
 minikube addons enable metallb
 
-#metalLB instalation && preparation 
-#kubectl get configmap kube-proxy -n kube-system -o yaml | \
-#sed -e "s/strictARP: false/strictARP: true/" | \
-#kubectl apply -f - -n kube-system
 
 eval $(minikube docker-env)
 
-<<<<<<< HEAD
-=======
-eval $(minikube -p minikube docker-env)
-
-#metalLB instalation && preparation 
-# kubectl get configmap kube-proxy -n kube-system -o yaml | \
-# sed -e "s/strictARP: false/strictARP: true/" | \
-# kubectl apply -f - -n kube-system
-
->>>>>>> e3c6c445707464786619b696469b758e7fd9e7f5
 
 echo "\n${Green}Installing metalLB...${endColour}"
 kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.5/manifests/namespace.yaml
@@ -66,21 +39,21 @@ kubectl apply -f srcs/loadbalancer/service.yml
 #building docker images
 echo "${Green}Building Docker images${endColour}"
 docker build -t my_nginx ./srcs/nginx --network host
+docker build -t my_ftps  ./srcs/ftps  --network host
+
 
 
 
 #Deployment and Services
 echo "${Green}Minikube Deployment and Services${endColour}"
 kubectl apply -f srcs/nginx/nginx.yml
+kubectl apply -f srcs/ftps/ftps.yml
 kubectl apply -f srcs/loadbalancer/metallb-config.yml
+kubectl apply -f srcs/loadbalancer/service.yml
 
 
-<<<<<<< HEAD
 #configuration file load balancer 
-#kubectl apply -f srcs/loadbalancer/service.yml
 
-=======
->>>>>>> e3c6c445707464786619b696469b758e7fd9e7f5
 
 #open k8s dashboard
 echo "\n${Green}Starting dashboard....${endColour}"
