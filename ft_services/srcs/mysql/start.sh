7a376353dc15d2c58b7797036cf3345b7270160d
Pablo Torres <ptorres@c2r15s1.42madrid.com>
2021-04-21 10:40:51 +0200

    services

--- ft_services/srcs/mysql/start.sh
+++ ft_services/srcs/mysql/start.sh
@@ -1,6 +1,13 @@
 rc-status
 /etc/init.d/mariadb setup 
 /etc/init.d/mariadb start 
-mariadb -u root  < wp.sql 
-while true; do sleep 30; done;
-
+mariadb -u root  < wp.sql
+while true 
+do
+	if [[ $(pgrep mysql) ]]
+	then
+		sleep 1;
+	else 
+		exit
+	fi
+done
