3c7f8c45eb39e8370a13453c156a3f40307ae93a
Pablo Torres <ptorres@c1r12s5.42madrid.com>
2022-01-20 13:39:21 +0100

    module08

--- cpp_modules/module08/ex01/span.hpp
+++ cpp_modules/module08/ex01/span.hpp
@@ -6,7 +6,22 @@
 /*   By: ptorres <ptorres@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/20 13:31:56 by ptorres           #+#    #+#             */
-/*   Updated: 2022/01/20 13:31:56 by ptorres          ###   ########.fr       */
+/*   Updated: 2022/01/20 13:38:06 by ptorres          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef SPAN_CPP
+#define SAPN_CPP
+
+class Span { 
+
+    private:
+        const unsigned int _N;
+        int  *nums;
+    public:
+        Span(unsigned int n);
+        Span();
+
+}
+
+#endif
