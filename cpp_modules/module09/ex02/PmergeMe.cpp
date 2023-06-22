7dd6896f5e19bb316526cfe44a13fdccb6e39128
pablo <pablots89@gmail.com>
2023-06-22 12:56:57 +0200

    ex02 reworked

--- cpp_modules/module09/ex02/PmergeMe.cpp
+++ cpp_modules/module09/ex02/PmergeMe.cpp
@@ -3,84 +3,206 @@
 /*                                                        :::      ::::::::   */
 /*   PmergeMe.cpp                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ptorres <ptorres@student.42.fr>            +#+  +:+       +#+        */
+/*   By: pablo <pablo@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/20 19:39:25 by pablo             #+#    #+#             */
-/*   Updated: 2023/06/21 20:56:51 by ptorres          ###   ########.fr       */
+/*   Updated: 2023/06/22 12:56:35 by pablo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "PmergeMe.hpp"
 
-PmergeMe::PmergeMe(char** args):_args(args)
+PmergeMe::PmergeMe(char **args) : _args(args)
 {
-  _readArgs();
-  _sortVec();
-  //_sortList();
-  _printVec();
-  //_printList();
+  _VectorReadArgs();
+  _ListReadArgs();
+  std::cout << "Before: ";
+  print(_vec);
+  timeVec = _VecMergeInsertSort(0, _vec.size());
+  timeList = _ListMergeInsertSort(_list.begin(), _list.end());
+  std::cout << "After: ";
+  print(_list);
+  std::cout << std::fixed << std::setprecision(10);
+  std::cout << "Time to process a range of " << _vec.size() << " with std::vector => " << timeVec << " s" << std::endl;
+  std::cout << "Time to process a range of " << _list.size() << " with std::list => " << timeList << " s" << std::endl;
 }
 
-//Insertion sort
-void PmergeMe::_sortVecpairs()
+bool is_valid_number(std::string number) {
+  return !number.empty() && number.find_first_not_of("0123456789") == std::string::npos;
+}
+void PmergeMe::_ListReadArgs()
 {
-  int n = std::distance(_vec.begin(), _vec.end());
-  for(int i = 1; i < n - 1;  i+=2)
-  {
-    
-    for (int j = i - 2; i > 1; i-=2)
-    if (*it > *(it + 2))
-    {
-      std::iter_swap(it, it + 2);
-      std::iter_swap(it + 1, it + 3);
-    }
-  }
-  _sortVecpairs(it + 2);
-}
-
-void PmergeMe::_sortVec() {
-  bool is_odd = std::distance(_vec.begin(), _vec.end()) % 2 != 0;
-  int last = 0;
-  if (is_odd)
-    last = *(_vec.end() - 1);
-  
-  Vecit it;
-  it = _vec.begin();
-  // Create pairs
-  while((it != _vec.end() && it != (_vec.end() - 1))) { 
-    if (*it > *(it + 1))
-      std::iter_swap(it, it + 1);
-    it+=2;
-  }
-  _printVec();
-  _sortVecpairs(_vec.begin(), std::distance(_vec.begin(), _vec.end()));
-}
-
-void PmergeMe::_printVec()
-{
-  Vecit it;
-  for(it = _vec.begin(); it != _vec.end(); it++)
-    std::cout << *it << ", ";
-  std::cout << std::endl;
-}
-
-void PmergeMe::_readArgs(){
   int number;
   for (int i = 1; _args[i]; i++)
-  { 
-    try
+  {
+    if (is_valid_number(std::string(_args[i])))
+    {
+      number = std::atoi(_args[i]),
+      _list.push_back(number);
+    }
+    else
+    {
+      std::cout << "Error: invalid number" << std::endl;
+      exit(1);
+    }
+  }
+}
+
+void PmergeMe::_VectorReadArgs()
+{
+  int number;
+  for (int i = 1; _args[i]; i++)
+  {
+    if (is_valid_number(std::string(_args[i])))
     {
       number = std::atoi(_args[i]),
       _vec.push_back(number);
-      _list.push_back(number);
     }
-    catch(...)
+    else
     {
-     std::cout << "Error: invalid number" << std::endl;
+      std::cout << "Error: invalid number" << std::endl;
+      exit(1);
     }
   }
 }
-  
+
+void PmergeMe::_ListInsertion(Iterator begin, Iterator end)
+{
+  for (Iterator i = begin; i != end; ++i)
+  {
+    Iterator j = i;
+    while (j != begin && *j < *(--j))
+      std::iter_swap(j, (++j)--);
+  }
+}
+
+void PmergeMe::_VecInsertion(int begin, int end)
+{
+  for (int i = begin; i <= end; i++) {
+        int aux = _vec[i];
+        int j = i - 1;
+        while (j >= begin && _vec[j] > aux) {
+            _vec[j + 1] = _vec[j];
+            j--;
+        }
+        _vec[j + 1] = aux;
+    }
+}
+
+void PmergeMe::_ListMerge(Iterator begin, Iterator mid, Iterator end)
+{
+  std::vector<int> temp;
+
+  Iterator i = begin;
+  Iterator j = mid;
+  while (i != mid && j != end)
+  {
+    if (*i <= *j)
+    {
+      temp.push_back(*i);
+      ++i;
+    }
+    else
+    {
+      temp.push_back(*j);
+      ++j;
+    }
+  }
+  while (i != mid)
+  {
+    temp.push_back(*i);
+    ++i;
+  }
+  while (j != end)
+  {
+    temp.push_back(*j);
+    ++j;
+  }
+  std::copy(temp.begin(), temp.end(), begin);
+}
+
+void PmergeMe::_VecMerge(int begin, int end, int mid)
+{
+   int i = begin, j = mid + 1, k = 0;
+    std::vector<int> temp(end - begin + 1);
+
+    while (i <= mid && j <= end) {
+        if (_vec[i] <= _vec[j])
+            temp[k++] = _vec[i++];
+        else
+            temp[k++] = _vec[j++];
+    }
+    while (i <= mid)
+        temp[k++] = _vec[i++];
+    while (j <= end)
+        temp[k++] = _vec[j++];
+    for (i = begin; i <= end; i++)
+        _vec[i] = temp[i - begin];
+}
+
+double PmergeMe::_ListMergeInsertSort(Iterator begin, Iterator end)
+{
+  std::clock_t t_start = std::clock();
+  if (std::distance(begin, end) <= 3)
+  {
+    _ListInsertion(begin, end);
+  }
+  else
+  {
+    Iterator mid = begin;
+    std::advance(mid, std::distance(begin, end) / 2);
+    _ListMergeInsertSort(begin, mid);
+    _ListMergeInsertSort(mid, end);
+    _ListMerge(begin, mid, end);
+  }
+  std::clock_t t_end = std::clock();
+  return double(t_end - t_start) / CLOCKS_PER_SEC;
+}
+
+double PmergeMe::_VecMergeInsertSort(int begin, int end) {
+  std::clock_t t_start = std::clock();
+  if (end - begin <= 2)
+      _VecInsertion(begin, end);
+  else {
+      int mid = begin + (end - begin) / 2;
+      _VecMergeInsertSort(begin, mid);  // sort first half
+      _VecMergeInsertSort(mid + 1, end);  // sort second half
+      _VecMerge(begin, end, mid);  // merge sorted halves
+  }
+  std::clock_t t_end = std::clock();
+  return double(t_end - t_start) / CLOCKS_PER_SEC;
+}
+
+template <typename T>
+void print(T container) { 
+  typename T::iterator it;
+  size_t counter = 0;
+  for(it = container.begin(); it != container.end() && counter < 6; it++)
+    std::cout << *it << " ", counter++;
+  if (it != container.end())
+    std::cout << "[...]";
+  std::cout << std::endl;
+}
+
+PmergeMe::PmergeMe()
+{
+}
+
+PmergeMe::PmergeMe(PmergeMe const &obj)
+{
+	*this = obj;
+	return ;
+}
+
+PmergeMe &PmergeMe::operator=(const PmergeMe &obj)
+{
+	this->_vec = obj._vec;
+  this->_list = obj._list;
+  this->_args = obj._args;
+  this->timeVec = obj.timeVec;
+  this->timeList = obj.timeList;
+	return (*this);
+}
 
 PmergeMe::~PmergeMe()
 {
--- cpp_modules/module09/ex02/PmergeMe.hpp
+++ cpp_modules/module09/ex02/PmergeMe.hpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   PmergeMe.hpp                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ptorres <ptorres@student.42.fr>            +#+  +:+       +#+        */
+/*   By: pablo <pablo@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/20 19:34:11 by pablo             #+#    #+#             */
-/*   Updated: 2023/06/21 20:56:23 by ptorres          ###   ########.fr       */
+/*   Updated: 2023/06/22 12:55:07 by pablo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,25 +16,35 @@
 #include <list>
 #include <iostream>
 #include <vector>
-
+#include <ctime>
+#include <iomanip>
+#include <regex>
 class PmergeMe
 {
 private:
-  typedef  std::vector<int>::iterator  Vecit;
-  typedef  std::vector<int>::iterator  Listit;
   std::vector<int>  _vec;
   std::list<int>    _list;
+  typedef std::list<int>::iterator Iterator;
   char**            _args;
-  void _readArgs();
-  void _printVec();
-  void _sortVec();
-  void _sortVecpairs();
-  //void _printList();
-  //void _sortList();
-  //void _printList();
+  void _VectorReadArgs();
+  void _ListReadArgs();
+  double timeVec;
+  double timeList;
+  void _ListInsertion(Iterator begin, Iterator end);
+  void _VecInsertion(int begin, int end);
+  void _VecMerge(int begin, int end, int mid);
+  void _ListMerge(Iterator begin, Iterator mid, Iterator end);
+  double _ListMergeInsertSort(Iterator begin, Iterator end);
+  double _VecMergeInsertSort(int begin, int end);
 public:
   PmergeMe(char** args);
+  PmergeMe();
+  PmergeMe(PmergeMe const &src);
+  PmergeMe &operator=(const PmergeMe &obj);
   ~PmergeMe();
 };
+
+template <typename T>
+void print(T container);
 
 #endif
